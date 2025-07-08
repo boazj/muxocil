1e14ce0429d763177e15fe4ac2e0226dc4715f50
Boaz Jan <boaz.jan@gmail.com>
2025-07-09 02:10:07 +0300

    unknown layout exit code

--- common/exit_codes.go
+++ common/exit_codes.go
@@ -1,10 +1,11 @@
 package common
 
 const (
-	ExitOk                = 0
-	ExitGeneralError      = 1
-	ExitConfigFailure     = 2
-	ExitConfigBindError   = 3
-	ExitOpenEditorError   = 4
-	ExitProviderDataError = 5
+	ExitOk                    = 0
+	ExitGeneralError          = 1
+	ExitConfigFailure         = 2
+	ExitConfigBindError       = 3
+	ExitOpenEditorError       = 4
+	ExitProviderDataError     = 5
+	ExitProviderUnknownLayout = 6
 )
